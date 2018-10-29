83b643ed2987beb3e04d56d5ba1c058793450629
Богдан Кушнир <gametester132@mail.ua>
2018-10-29 17:57:30 +0300

    Тестовая справка

---
+++ DiscordBot/Loger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    class Loger : IDisposable
+    {
+        public Loger(string Path)
+        {
+            File = new FileStream(Path, FileMode.Create);
+            LogWriter = new StreamWriter(File);
+        }
+
+        FileStream File { get; }
+        StreamWriter LogWriter { get; }
+
+        public void Log(string LogText)
+        {
+            DateTime time = DateTime.Now;
+            string log = $"[{time.ToShortDateString()} | {time.ToShortTimeString()}] --- {LogText}";
+            Console.WriteLine(log);
+            LogWriter.WriteLine(log);
+        }
+
+        public Task LogAsync(string LogText)
+        {
+            return Task.Factory.StartNew(() => {
+                DateTime time = DateTime.Now;
+                string log = $"[{time.ToShortDateString()} | {time.ToShortTimeString()}] --- {LogText}";
+                Console.WriteLine(log);
+                LogWriter.WriteLine(log);
+            });
+        }
+
+        public void Dispose()
+        {
+            LogWriter.Dispose();
+            File.Dispose();
+        }
+
+        ~Loger()
+        {
+            Dispose();
+        }
+    }
+}
--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 using DSharpPlus;
+using System.Threading.Tasks;
+using DSharpPlus.Entities;
+using System.Threading;
 
 namespace DiscordBot
 {
     class Program
     {
+        static Loger Loger { get; set; } = new Loger($"Logs\\{DateTime.Now.ToShortDateString()} - {DateTime.Now.ToShortTimeString()}.log");
         static void Main(string[] args)
         {
             string token;
@@ -13,16 +18,91 @@ namespace DiscordBot
                 token = args[0];
             else
             {
-                Console.WriteLine("Введите токен для подключения:");
+                Console.WriteLine("Введите токен для бота:");
                 token = Console.ReadLine();
             }
 
-            Console.WriteLine($"Бот будет запущен по токену \" {token} \"");
+            Loger.Log($"Бот будет запущен по токену \"{token}\".");
 
-            DiscordClient discord = new DiscordClient(new DiscordConfiguration()
+            DiscordClient discord = new DiscordClient(new DiscordConfiguration
             {
                 Token = token,
                 TokenType = TokenType.Bot
+            });
+
+            discord.MessageCreated += Discord_MessageCreated;
+
+            discord.ConnectAsync().Wait();
+
+            Loger.Log($"Бот успешно подключён.");
+
+            while (true)
+            {
+                string command = Console.ReadLine().ToLower().Trim();
+
+                if (command == "exit")
+                    break;
+            }
+        }
+
+        private static Task Discord_MessageCreated(DSharpPlus.EventArgs.MessageCreateEventArgs e)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                //зачем терять время на неонносясиеся к нам сообщения?
+                if (!e.Message.Content.StartsWith("#"))
+                    return;
+
+                //пользовательское сообщение
+                DiscordMessage userMessage = e.Message;
+                //содержимое ответа
+                string messageText = $"{e.Message.Author.Mention} ";
+
+
+                #region Опредиление запроса
+                if (userMessage.Content.ToLower().StartsWith("#time"))
+                    messageText += $"{DateTime.Now.ToShortTimeString()}";
+
+                if (userMessage.Content.ToLower().StartsWith("#date"))
+                    messageText += $"{DateTime.Now.ToShortDateString()}";
+
+                if (userMessage.Content.ToLower().StartsWith("#code sort"))
+                {
+                    messageText += "```\n" +
+                    "public static void sorting(double[] arr, long first, long last)\n" +
+                    "{\n" +
+                        "\tdouble p = arr[(last - first) / 2 + first];\n" +
+                        "\tdouble temp;\n" +
+                        "\tlong i = first, j = last;\n" +
+                        "\twhile (i <= j)\n" +
+                        "\t{\n" +
+                            "\t\twhile (arr[i] < p && i <= last) ++i;\n" +
+                            "\t\twhile (arr[j] > p && j >= first) --j;\n" +
+                            "\t\tif (i <= j)\n" +
+                            "\t\t{" +
+                                "\t\t\ttemp = arr[i];\n" +
+                                "\t\t\tarr[i] = arr[j];\n" +
+                                "\t\t\tarr[j] = temp;\n" +
+                                "\t\t\t++i; --j;\n" +
+                            "\t\t}\n" +
+                        "\t}\n" +
+                        "\tif (j > first) sorting(arr, first, j);\n" +
+                        "\tif (i < last) sorting(arr, i, last);\n" +
+                    "}\n" +
+                    "```";
+                }
+                #endregion
+
+
+
+                //чтобы чат не засорять
+                userMessage.DeleteAsync();
+                //отправка ответа
+                DiscordMessage respondMessage = e.Message.RespondAsync(messageText).Result;
+                ////10 секунд на чтение
+                //Thread.Sleep(60000);
+                ////чтобы чат не засорять ботовским спамом
+                //respondMessage.DeleteAsync();
             });
         }
     }
